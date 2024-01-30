3876106552ed8b3296c6c9f837040231ec5aa67e
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-30 11:16:27 +0100

    Add files via upload

---
+++ Caso1.java
@@ -0,0 +1,10 @@
+package tokio.school;
+
+public class Caso1 {
+    public static void main(String []args){
+        Application1 application1 = new Application1();
+        application1.seeProducts();
+        application1.seeProductBetweenPrices(90,260);
+
+    }
+}
